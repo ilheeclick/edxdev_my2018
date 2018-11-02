015496d24c8f565e3a4f6ce16af5a85f7aaa02cc
92hoy <ho_9209@naver.com>
2018-11-02 07:56:16 +0000

    refactor: register-input , focus in/out delete

--- lms/static/js/student_account/views/RegisterView.js
+++ lms/static/js/student_account/views/RegisterView.js
@@ -170,70 +170,70 @@
                 },
 
                 postRender: function() {
-                    var inputs = this.$('.form-field'),
-                        inputSelectors = 'input, select, textarea',
-                        inputTipSelectors = ['tip error', 'tip tip-input'],
-                        inputTipSelectorsHidden = ['tip error hidden', 'tip tip-input hidden'],
-                        onInputFocus = function() {
-                            // Apply on focus styles to input
-                            $(this).find('label').addClass('focus-in')
-                                .removeClass('focus-out');
-
-                            // Show each input tip
-                            $(this).children().each(function() {
-                                if (inputTipSelectorsHidden.indexOf($(this).attr('class')) >= 0) {
-                                    $(this).removeClass('hidden');
-                                }
-                            });
-                        },
-                        onInputFocusOut = function() {
-                            // If input has no text apply focus out styles
-                            if ($(this).find(inputSelectors).val().length === 0) {
-                                $(this).find('label').addClass('focus-out')
-                                    .removeClass('focus-in');
-                            }
-
-                            // Hide each input tip
-                            $(this).children().each(function() {
-                                if (inputTipSelectors.indexOf($(this).attr('class')) >= 0) {
-                                    $(this).addClass('hidden');
-                                }
-                            });
-                        },
-                        handleInputBehavior = function(input) {
-                            // Initially put label in input
-                            if (input.find(inputSelectors).val().length === 0) {
-                                input.find('label').addClass('focus-out')
-                                    .removeClass('focus-in');
-                            }
-
-                            // Initially hide each input tip
-                            input.children().each(function() {
-                                if (inputTipSelectors.indexOf($(this).attr('class')) >= 0) {
-                                    $(this).addClass('hidden');
-                                }
-                            });
-
-                            input.focusin(onInputFocus);
-                            input.focusout(onInputFocusOut);
-                        },
-                        handleAutocomplete = function() {
-                            $(inputs).each(function() {
-                                var $input = $(this),
-                                    isCheckbox = $input.attr('class').indexOf('checkbox') !== -1;
-
-                                if (!isCheckbox) {
-                                    if ($input.find(inputSelectors).val().length === 0
-                                        && !$input.is(':-webkit-autofill')) {
-                                        $input.find('label').addClass('focus-out')
-                                            .removeClass('focus-in');
-                                    } else {
-                                        $input.find('label').addClass('focus-in')
-                                            .removeClass('focus-out');
-                                    }
-                                }
-                            });
-                        };
+                    // var inputs = this.$('.form-field'),
+                    //     inputSelectors = 'input, select, textarea',
+                    //     inputTipSelectors = ['tip error', 'tip tip-input'],
+                    //     inputTipSelectorsHidden = ['tip error hidden', 'tip tip-input hidden'],
+                    //     onInputFocus = function() {
+                    //         // Apply on focus styles to input
+                    //         $(this).find('label').addClass('focus-in')
+                    //             .removeClass('focus-out');
+                    //
+                    //         // Show each input tip
+                    //         $(this).children().each(function() {
+                    //             if (inputTipSelectorsHidden.indexOf($(this).attr('class')) >= 0) {
+                    //                 $(this).removeClass('hidden');
+                    //             }
+                    //         });
+                    //     },
+                    //     onInputFocusOut = function() {
+                    //         // If input has no text apply focus out styles
+                    //         if ($(this).find(inputSelectors).val().length === 0) {
+                    //             $(this).find('label').addClass('focus-out')
+                    //                 .removeClass('focus-in');
+                    //         }
+                    //
+                    //         // Hide each input tip
+                    //         $(this).children().each(function() {
+                    //             if (inputTipSelectors.indexOf($(this).attr('class')) >= 0) {
+                    //                 $(this).addClass('hidden');
+                    //             }
+                    //         });
+                    //     },
+                    //     handleInputBehavior = function(input) {
+                    //         // Initially put label in input
+                    //         if (input.find(inputSelectors).val().length === 0) {
+                    //             input.find('label').addClass('focus-out')
+                    //                 .removeClass('focus-in');
+                    //         }
+                    //
+                    //         // Initially hide each input tip
+                    //         input.children().each(function() {
+                    //             if (inputTipSelectors.indexOf($(this).attr('class')) >= 0) {
+                    //                 $(this).addClass('hidden');
+                    //             }
+                    //         });
+                    //
+                    //         input.focusin(onInputFocus);
+                    //         input.focusout(onInputFocusOut);
+                    //     },
+                    //     handleAutocomplete = function() {
+                    //         $(inputs).each(function() {
+                    //             var $input = $(this),
+                    //                 isCheckbox = $input.attr('class').indexOf('checkbox') !== -1;
+                    //
+                    //             if (!isCheckbox) {
+                    //                 if ($input.find(inputSelectors).val().length === 0
+                    //                     && !$input.is(':-webkit-autofill')) {
+                    //                     $input.find('label').addClass('focus-out')
+                    //                         .removeClass('focus-in');
+                    //                 } else {
+                    //                     $input.find('label').addClass('focus-in')
+                    //                         .removeClass('focus-out');
+                    //                 }
+                    //             }
+                    //         });
+                    //     };
 
                     FormView.prototype.postRender.call(this);
                     $('.optional-fields').addClass('hidden');
